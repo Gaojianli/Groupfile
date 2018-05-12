6a4d8291e211f0e006b2bfa79685985cec682406
Gaojianli <823805669@qq.com>
2018-05-12 13:33:03 +0800

    login impletment
    the impletment of login

--- client/app.js
+++ client/app.js
@@ -1,35 +1,11 @@
 //app.js
 App({
-  loginStatus: false,
   errReason: "",
   //错误退出实现
   quitFlag: false,
 
   onLaunch: function () {
     // 登录
-    wx.login({
-      success: res => {
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: 'https://asdf.zhr1999.club/api/login',
-            data: {
-              code: res.code
-            },
-            method: "GET",
-            success: res => {
-              if (res) {
-                console.log(res.data)
-              }
-            }
-          })
-        } else {
-          console.log('登录失败！' + res.errMsg)
-          loginStatus: false
-        }
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    }),
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -41,6 +17,9 @@ App({
               }
             }
           })
+        }
+        else{
+          loginCus()
         }
       }
     })
--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,34 +1,38 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+var util = require("../../utils/util.js")
+import regeneratorRuntime from "../../utils/runtime.js"
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
+    loginStatus: false,
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    filelist:{
-      empty:true,
-      data:[]
+    filelist: {
+      empty: true,
+      data: []
     }
   },
   //事件处理函数
-  onLoad: function () {
-    if(!app.loginStatus)
-    {
-      wx.showToast({
-        title: '登录失败！',
-        icon: 'loading',
-        duration: 1500,
-        success:()=>{
-           // wx.reLaunch({
-          //   url: '../pages/exit/exit',
-          // })
-          console.log("退出");
-        }
-      })
-    }
+  onLoad: async function () {
+    this.setData({
+      filelist: {
+        empty: false,
+        data: [
+          {
+            id: 1,
+            fileName: "我是文件名",
+            uploadTime: "2018-2-3 15：32"
+          },
+          {
+            id: 2,
+            fileName: "我是文件二号",
+            uploadTime: "time"
+          }
+        ]
+      }
+    })
     //获取用户信息
     if (app.globalData.userInfo) {
       this.setData({
@@ -53,26 +57,26 @@ Page({
         }
       })
     }
+    if (!app.loginStatus) {
+      await loginCus(this);
+      wx.showToast({
+        title: '登录失败！',
+        icon: 'loading',
+        duration: 1500,
+        success: () => {
+          // wx.reLaunch({
+          //   url: '../pages/exit/exit',
+          // })
+          console.log("退出");
+        }
+      })
+    }
+    else{
+
+    }
     //获取文件列表
-    this.setData({
-      filelist:{
-        empty:false,
-        data:[
-          {
-            id:1,
-            fileName:"我是文件名",
-            uploadTime:"2018-2-3 15：32"
-          },
-          {
-            id:2,
-            fileName:"我是文件二号",
-            uploadTime:"time"
-          }
-        ]
-      }
-    })
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -81,3 +85,38 @@ Page({
     })
   },
 })
+
+const loginCus = (that) => {
+  return new Promise(resolve => {
+    wx.login({
+      success: res => {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'https://asdf.zhr1999.club/api/login',
+            data: {
+              code: res.code
+            },
+            method: "GET",
+            success: res => {
+              if (res) {
+                console.log(res.data)
+                that.setData({
+                  loginStatus: true,
+                })
+                console.log(that);
+              }
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+          loginStatus: false
+        }
+      }
+    })
+  })
+}
+
+const getFileList=async cookie=>{
+  
+}
