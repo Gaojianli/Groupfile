3074b2f0c81090e319248da351c42ff0455f3f92
Gaojianli <823805669@qq.com>
2018-05-12 11:54:34 +0800

    try to login
    try to login

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -14,20 +14,28 @@ Page({
     }
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onLoad: function () {
+    if(!app.loginStatus)
+    {
+      wx.showToast({
+        title: '登录失败！',
+        icon: 'loading',
+        duration: 1500,
+        success:()=>{
+           // wx.reLaunch({
+          //   url: '../pages/exit/exit',
+          // })
+          console.log("退出");
+        }
+      })
+    }
+    //获取用户信息
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
+    } else if (this.data.canIUse) {
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -35,7 +43,6 @@ Page({
         })
       }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
