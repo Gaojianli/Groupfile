122f76c45e19adacc7f704815b1acec326265d98
xice <liangxiao0991@gmail.com>
2018-05-20 18:04:29 +0800

    change the data style

--- servers/controllers/getFileList.js
+++ servers/controllers/getFileList.js
@@ -32,6 +32,21 @@ COOKIE失效
 let session_token = require('../sql/session');
 let file = require('../sql/file');
 let user_info = require('../sql/user');
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
 module.exports = async(ctx, next) => {
     let post = ctx.request.body;
     let user = await session_token.get_user(post.session_cookie);
@@ -49,6 +64,10 @@ module.exports = async(ctx, next) => {
     let empty=true;
     if(files){
         empty = false;
+    }
+    for (const i of files) {
+        let d = new Date(i.upload_time);
+        i.upload_time = d.Format('yy-MM-dd hh:mm');
     }
     ctx.response.body = JSON.stringify({success:true,files:files,empty:empty});
 }
--- servers/controllers/openShare.js
+++ servers/controllers/openShare.js
@@ -1,17 +1,23 @@
 /*
 API openShare
 调用方式 POST
-调用类型 JSON
+调用类型 post
 传参 
-    {
-        "session_cookie": "YOUR SESSION COOKIE",
-        "openGid": "SHEAR openGid",
-        "file_id": "file_id"
-    }
+    "session_cookie": "YOUR SESSION COOKIE",
+    "openGid": "SHEAR openGid",
+    "file_id": "file_id"
 返回
     成功：
     {
         "success": true,
-        
+        "file": {
+            "file_id": "THE FILE ID",
+            "name": "THE FILE NAME",
+            "upload_time": "",
+            "download_num": 10
+        }
     }
 */
+let user = require('../sql/user');
+let group_info = require('../sql/group');
+
