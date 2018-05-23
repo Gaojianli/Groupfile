e046059440ce69ebb16c7e35ec7666bf47afa59c
xice <liangxiao0991@gmail.com>
2018-05-23 17:18:34 +0800

    I forget to add the route,add it

--- servers/route/route.js
+++ servers/route/route.js
@@ -7,5 +7,6 @@ module.exports = compose([
     route.post('/api/log',require('../controllers/log')),
     route.get('/api/login',require('../controllers/login')),
     route.post('/api/getFileList',require('../controllers/getFileList')),
-    route.post('/api/openShare',require('../controllers/openShare'))
+    route.post('/api/openShare',require('../controllers/openShare')),
+    route.post('/api/getFileInfo',require('../controllers//getFileInfo'))
 ]);
