fa425ef37a43c50f05352cbc9c058a30a4c33c7f
xice09 <liangxiao09@163.com>
2018-06-01 10:18:43 +0800

    test if the websocket get in the route,can the server work?

--- servers/app.js
+++ servers/app.js
@@ -1,18 +1,19 @@
 let Koa = require("koa");
 // let bodyParser = require('koa-bodyparser');
+let websocket = require('koa-websocket')
 let bodyParser = require('koa-body');
 let config = require('./config');
 let route = require('./route/route');
-const app = new Koa();
-app.use(bodyParser({multipart: true}));
+const app = websocket(new Koa());
+app.use(bodyParser({ multipart: true }));
 app.use(async(ctx, next) => {
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
 })
-app.use(async(ctx, next)=>{
-    if(ctx.request.method == "POST"){
+app.use(async(ctx, next) => {
+    if (ctx.request.method == "POST") {
         // for(ctx.request.body in ctx.request.body);
-        if(global.conf.debug) console.log(ctx.request.body);
+        if (global.conf.debug) console.log(ctx.request.body);
     }
     await next();
 })
