caa57b60dcdca64cfd2fa05c359c74b1302276b5
xice <liangxiao0991@gmail.com>
2018-06-06 21:22:02 +0800

    init it and I lean the vue basic OK.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,21 +1,30 @@
 <template>
-  <div id="app">
-    <header></header>
-    <img src="./assets/logo.png">
-    <router-view/>
-    <foote></foote>
-  </div>
+  <mu-flex direction="column" justify-content="between" id="app">
+    <mu-appbar style="width: 100%;" color="primary">
+      <!-- <mu-button icon slot="left">
+        <mu-icon value="menu"></mu-icon>
+      </mu-button> -->
+      {{Title}}
+      <!-- <mu-button flat slot="right">LOGIN</mu-button> -->
+    </mu-appbar>
+    <router-view style="width: 100%;"/>
+    <mu-flex class="footer" style="width: 100%;" direction="column" align-items="center">
+      <mu-flex class="footText" justify-content="center"> 版权信息 </mu-flex>
+      <mu-flex class="footText" justify-content="center"> 版权信息 </mu-flex>
+    </mu-flex>
+  </mu-flex>
 </template>
 
 <script>
-import Header from './components/Header'
-import Footer from './components/Footer'
+
 export default {
   name: 'App',
-  components:{
-    Header,
-    Footer
-  }
+  data(){
+    return {
+      Title: "群文件"
+    }
+  },
+  components:{ }
 }
 </script>
 
@@ -26,6 +35,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height:100%;
+}
+.footer{
+  border-top:1px solid gray;
+  box-shadow: darkgrey 3px 0 0 0;
+}
+.footText{
+  color: gray;
+  font-size: 80%;
 }
 </style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import qrCode from '@/components/qrCode'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'qrCode',
+      component: qrCode
+    },
+    {
+      path: '/HelloWord',
+      name: 'HelloWord',
       component: HelloWorld
     }
   ]
