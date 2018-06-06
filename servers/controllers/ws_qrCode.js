e5b6940a922be45c8ed46112ff5768de3ed2510c
xice <liangxiao0991@gmail.com>
2018-06-07 00:10:13 +0800

    fix the ws async bug

--- servers/controllers/ws_qrCode.js
+++ servers/controllers/ws_qrCode.js
@@ -1,6 +1,6 @@
 let session_token = require('../sql/session');
 module.exports = async(ctx) => {
-    let cookie = await session_token.new_cookie("web", "");
+    let cookie = await session_token.new_cookie("web", null);
     global.data[cookie] = ctx.websocket;
     ctx.websocket.send(global.conf.root + "/api/scanCode?cookie=" + cookie);
     ctx.websocket.onclose = () => {
