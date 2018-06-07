6aa5a4f61eb4639ac60177895becbb1349b8c3dc
xice09 <liangxiao09@163.com>
2018-06-08 03:58:13 +0800

    add some tmp header to test easy

--- servers/app.js
+++ servers/app.js
@@ -5,6 +5,9 @@ let bodyParser = require('koa-body');
 let config = require('./config');
 let route = require('./route/route');
 const app = websocket(new Koa());
+// WARRING: THIS CONFIG IS DANGOURS, MAST MAKESURE IT BE REMOVE WHITOUT TEST.
+let cors = require('koa-cors');
+app.use(cors());
 app.ws.use(route.ws);
 app.use(bodyParser({ multipart: true }));
 app.use(async(ctx, next) => {
@@ -19,11 +22,6 @@ app.use(async(ctx, next) => {
         ctx.response.status = 200;
 
     }
-    // WARRING: THIS CONFIG IS DANGOURS, MAST MAKESURE IT BE REMOVE WHITOUT TEST.
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By", ' 3.2.1')
     await next();
 })
 app.use(route.common);
