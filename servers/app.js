195a72ca60ff39683e9a6023c3fd7d82194d9d63
xice09 <liangxiao09@163.com>
2018-06-08 03:53:25 +0800

    add some tmp header to test easy

--- servers/app.js
+++ servers/app.js
@@ -17,7 +17,13 @@ app.use(async(ctx, next) => {
         if (global.conf.debug) console.log(ctx.request.body);
     } else if (ctx.request.method == "OPTIONS") {
         ctx.response.status = 200;
+
     }
+    // WARRING: THIS CONFIG IS DANGOURS, MAST MAKESURE IT BE REMOVE WHITOUT TEST.
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
     await next();
 })
 app.use(route.common);
