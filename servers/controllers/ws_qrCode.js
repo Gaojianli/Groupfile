3ae956d5e41346621d19c9f57e68ef2cd1eb4fd7
xice09 <liangxiao09@163.com>
2018-06-08 19:18:50 +0800

    try to use ws to flish filesList

--- servers/controllers/ws_qrCode.js
+++ servers/controllers/ws_qrCode.js
@@ -3,7 +3,7 @@ module.exports = async(ctx) => {
     let cookie = await session_token.new_cookie("web", null);
     global.ws.qr_scan[cookie] = ctx.websocket;
     ctx.websocket.send(JSON.stringify({ success: "get_qrCode", qrurl: global.conf.root + "/api/scanCode?cookie=" + cookie }));
-    ctx.websocket.onclose = () => {
+    ctx.websocket.on('close', () => {
         delete global.ws.qr_scan[cookie];
-    }
+    })
 }
--- servers/controllers/ws_uploadListen.js
+++ servers/controllers/ws_uploadListen.js
@@ -1,7 +1,7 @@
 let session_cookie = require('../sql/session');
 module.exports = async(ctx) => {
     var cookie = null;
-    ctx.websocket.onmessage = (rec) => {
+    ctx.websocket.on("message", (rec) => {
         let user = session_cookie.get_user(rec.data);
         if (!user) {
             ctx.websocket.send(JSON.stringify({ success: "error" }))
@@ -10,10 +10,10 @@ module.exports = async(ctx) => {
         cookie = rec.data;
         global.ws.upload[cookie] = ctx.websocket;
         ctx.websocket.send(JSON.stringify({ success: "uploadListen" }))
-    }
-    ctx.websocket.onclose = () => {
+    })
+    ctx.websocket.on("close", () => {
         if (cookie) {
             delete global.ws.qr_scan[cookie];
         }
-    }
+    })
 }
